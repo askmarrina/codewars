9ddca8e0c71bb71d211968f66e26a0969eb38eed
Marina K <askmarrina@gmail.com>
2025-07-03 19:17:03 +0200

    reversed-strings task

---
+++ 8kyu/reversed-strings/main.js
@@ -0,0 +1,8 @@
+// complete the solution so that it reverses the string passed into it
+// 'world' => 'dlrow'
+// 'word' => 'drow'
+
+//SOLUTION:
+function solution(str) {
+    return str.split('').reverse().join('');
+}
