b506e57d38ba9cb23f72d23bfff66f6b5261e721
Marina K <askmarrina@gmail.com>
2025-07-03 19:41:19 +0200

    completing multiply task

---
+++ 8kyu/multiply/multiply.js
@@ -0,0 +1,13 @@
+// ❓DESCRIPTION
+// This code does not execute properly. Try to figure out why.
+
+// ✅SOLUTION
+
+function multiply(a, b){
+    let multi;
+    multi = a * b;
+    return multi;
+}
+
+console.log(multiply(3, 2))
+
