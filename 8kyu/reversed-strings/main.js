51ef7c29bb9bc2ef46ce5a8db8a37e774d6b7fc3
Marina K <askmarrina@gmail.com>
2025-07-03 19:23:10 +0200

    final commit in task

--- 8kyu/reversed-strings/main.js
+++ 8kyu/reversed-strings/main.js
@@ -1,8 +1,11 @@
+// ❓ DESCRIPTION
 // complete the solution so that it reverses the string passed into it
 // 'world' => 'dlrow'
 // 'word' => 'drow'
 
-//SOLUTION:
+// ✅ SOLUTION:
 function solution(str) {
     return str.split('').reverse().join('');
 }
+
+console.log("solution: ", solution('world'))
