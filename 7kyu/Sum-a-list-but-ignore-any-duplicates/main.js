24103e8bb215f3066df174ee7fb7298e33c1e04c
Marina K <askmarrina@gmail.com>
2025-09-01 13:08:57 +0200

    7kyu task codewars is solved

---
+++ 7kyu/Sum-a-list-but-ignore-any-duplicates/main.js
@@ -0,0 +1,24 @@
+// Please write a function that sums a list, but ignores any duplicated items in the list.
+//
+//     For instance, for the list [3, 4, 3, 6] the function should return 10,
+//     and for the list [1, 10, 3, 10, 10] the function should return 4.
+
+
+function sumNoDuplicates(numList) {
+    const uniqueElements = numList.filter(function(element) {
+        let count = 0; // считаем, сколько раз встречается этот элемент
+        for(let i = 0; i < numList.length; i++) {
+            if(numList[i] === element) {
+                count++;
+            }
+        }
+        return count === 1; // оставляем только элементы, которые встречаются один раз
+    })
+
+    let sum = 0;
+    for(let i = 0; i < uniqueElements.length; i++) {
+        sum += uniqueElements[i];
+    }
+    return sum;
+}
+console.log(sumNoDuplicates([1, 2, 3, 4, 4]));
