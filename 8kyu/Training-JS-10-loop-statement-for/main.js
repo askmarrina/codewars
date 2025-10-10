0a928c862b6245edac5c90944d27f1c83a95d1d2
askmarrina <askmarrina@gmail.com>
2025-10-10 19:19:01 +0200

    completed 8kyu task about for loop

---
+++ 8kyu/Training-JS-10-loop-statement-for/main.js
@@ -0,0 +1,22 @@
+// Task
+// The function pickIt accepts 1 parameter, arr, which is an array of numbers. We need to traverse arr by using a for loop. If an element is an odd number, push it to the odd array, and if it's an even number, push it to the evenarray.
+//
+// I've defined two arrays odd and even in the function, and also wrote the return statement. Your work is to write a for loop.
+
+
+function pickIt(arr){
+    let odd = [];
+    let even = [];
+    //coding here
+    for (let i=0; i<arr.length; i++) {
+        if(arr[i] % 2 === 0){
+            even.push(arr[i])
+        } else {
+            odd.push(arr[i])
+        }
+    }
+
+    return [odd,even];
+}
+
+console.log(pickIt([1,2,3,4,5,6,7,8,9,10]));
