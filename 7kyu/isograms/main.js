30903b43c5e8418e273bf243fd578112aec55719
askmarrina <askmarrina@gmail.com>
2025-11-25 00:29:59 +0100

    7kyu task is completed

---
+++ 7kyu/isograms/main.js
@@ -0,0 +1,17 @@
+function isIsogram(str){
+    //...
+    str = str.toLowerCase();
+    for(let i = 0; i<str.length; i++){
+        for(let j = i+1; j<str.length; j++){
+            if(str[i] === str[j]) {
+                return false
+            }
+        }
+    }
+    return true
+}
+
+console.log(isIsogram("Dermatoglyphics"));
+console.log(isIsogram("aba"));
+console.log(isIsogram("moOse"));
+console.log(isIsogram(""));
